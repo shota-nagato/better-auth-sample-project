3a96b7b43929e4b29ffc34b15f461a0d3c7e7201
shota-nagato <shota.nagato@gmail.com>
2025-11-05 23:12:04 +0900

    google login

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,6 +5,12 @@ import { nextCookies } from "better-auth/next-js";
 import { schema } from "@/db/schema";
 
 export const auth = betterAuth({
+    socialProviders: {
+      google: {
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      }
+    },
     emailAndPassword: {
       enabled: true
     },
